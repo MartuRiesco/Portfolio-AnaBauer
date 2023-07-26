20f4615e372460a20e12720060803360c3e632d1
Martu Riesco <marturiesco.costa@hotmail.com>
2023-07-26 19:43:19 -0300

    adding components to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import FotoAnalogica from './components/FotoAnalogica/FotoAnalogica';
+import Reel from './components/Reel/Reel';
+import Video from './components/Video/Video';
 
 function App() {
   document.oncontextmenu = function () {
@@ -14,7 +16,9 @@ function App() {
 
       <main>
         <Routes>
-         
+         <Route path='/fotoanalogica' element={<FotoAnalogica/>}/>
+         <Route path='/reel' element={<Reel/>}/>
+         <Route path='/video' element={<Video/>}/>
         </Routes>
       </main>
      
