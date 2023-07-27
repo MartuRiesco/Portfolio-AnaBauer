dceb2dda3dab52f34cfad68497f6fabf1f4ef7e1
Martu Riesco <marturiesco.costa@hotmail.com>
2023-07-27 11:10:44 -0300

    add FotoAnalogicand Reel components and styles

--- src/components/FotoAnalogica/FotoAnalogica.jsx
+++ src/components/FotoAnalogica/FotoAnalogica.jsx
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { getFotoAnalogica } from '../../services/firebase';
-import Grid from '@mui/material/Grid';
-import Item from '@mui/material/ListItem'
+import { IoMdClose } from 'react-icons/io';
+import ImageListItem from '@mui/material/ImageListItem'
+import { ImageList } from '@mui/material';
 import './style.css'
+
 
 
 function FotoAnalogica() {
     const [Users, setUser] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setModalOpen(false);
+  };
     async function leerDatos() {
         let respuesta = await getFotoAnalogica();
         setUser(respuesta);
@@ -17,20 +31,37 @@ function FotoAnalogica() {
       }, []);
   return (
     <div className='grilla-fotos'>
-        <Grid container spacing={1}>
+         {modalOpen && (
+        <div className="modal-bk">
+          <div className='modal-cont'>
+          <IoMdClose onClick={closeModal} className="modal-close-button">
+            Cerrar
+          </IoMdClose>
+          <img
+            src={selectedImage}
+            alt="Imagen seleccionada"
+            className="modal-image"
+          />
+          </div>
+        </div>
+      )}
+        <ImageList variant="masonry" cols={2} gap={80}>
         {Users.map((user)=>(
-             <Grid item xs="auto">
-                 <Item>
-                    <img src={user.img} alt={user.category} className='fotogrilla' />
-                 </Item>
-             </Grid>
+             <ImageListItem key={user.img}>
+             <img
+               src={`${user.img}?w=248&fit=crop&auto=format`}
+               srcSet={`${user.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+               alt={user.category}
+               loading="lazy"
+               className='fotogrilla'
+               onClick={() => openModal(user.img)}
+             />
+              
+           </ImageListItem>
         ))}
-
-
-        </Grid>
+        </ImageList>
 
     </div>
   )
 }
-
 export default FotoAnalogica
--- src/components/Reel/Reel.jsx
+++ src/components/Reel/Reel.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Reel() {
   return (
-    <div>Reel</div>
+    <div>
+     <iframe src="https://player.vimeo.com/video/848918316?h=97737faf66" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+
+     </iframe>
+<p>
+  <a href="https://vimeo.com/848918316">
+    Reel - Ana Bauer 2023
+  </a> 
+  from 
+  <a href="https://vimeo.com/user202548570">Ana Bauer</a> 
+  on <a href="https://vimeo.com">Vimeo</a>.</p>
+      </div>
   )
 }
 
