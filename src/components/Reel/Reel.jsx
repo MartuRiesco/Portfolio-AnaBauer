99ef960a5100b87403891f1fad4aedbbdf971a1f
Martu Riesco <marturiesco.costa@hotmail.com>
2023-07-28 10:52:06 -0300

    styling reel component

--- src/components/Reel/Reel.jsx
+++ src/components/Reel/Reel.jsx
@@ -4,17 +4,14 @@ import './style.css'
 function Reel() {
   return (
     <div className='reel'>
-      <h1 className='titulo-reel'> REEL</h1>
-     <iframe src="https://player.vimeo.com/video/848918316?h=97737faf66" width="900" height="500" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
-
+      
+      <div className='titulo-container'>
+     <hr className='hr1' /> <h2 className='titulo-reel'> REEL</h2></div>
+     <div className=' iframe-reel'>
+     <iframe  src="https://player.vimeo.com/video/848918316?h=97737faf66" width="900" height="500" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+     
      </iframe>
-<p>
-  <a href="https://vimeo.com/848918316" className='descrip-reel'>
-    Reel - Ana Bauer 2023
-  </a> 
-  from 
-  <a href="https://vimeo.com/user202548570" className='descrip-reel'>Ana Bauer</a> 
-  on </p>
+     </div>
       </div>
   )
 }
